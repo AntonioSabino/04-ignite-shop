0a0b7765f787d0569c251f0f652dfdf233656c67
Antonio Sabino <aslimajr@outlook.com>
2023-03-25 16:01:22 -0300

    feat: estruturando a página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,27 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails
+} from '@/styles/pages/product'
+
 export default function Product() {
   return (
-    <>
-      <h1>Products</h1>
-    </>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero alias
+          velit sed dicta quibusdam illum quam? In aperiam vero doloribus, vitae
+          distinctio aspernatur impedit? Animi quam maxime placeat itaque
+          voluptates.
+        </p>
+      </ProductDetails>
+
+      <button>Comprar agora</button>
+    </ProductContainer>
   )
 }
