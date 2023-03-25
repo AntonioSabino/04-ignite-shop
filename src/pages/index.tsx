0b49a4fb8d40620ca1571862a499f60b3bad6023
Antonio Sabino <aslimajr@outlook.com>
2023-03-25 13:02:02 -0300

    feat: estruturando a Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,27 @@
 import { styled } from '@/styles'
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: '4px',
-  border: 0,
-  padding: '4px 8px',
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
 
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+const camisetas = [camiseta1, camiseta2, camiseta3]
 
 export default function Home() {
   return (
     <>
-      <Button>
-        <span>Teste </span>
-        Enviar
-      </Button>
+      <HomeContainer>
+        {camisetas.map((camiseta, index) => (
+          <Product key={index}>
+            <Image src={camiseta} width={520} height={480} alt="" />
+            <footer>
+              <strong>Camiseta X</strong>
+              <span>R$ 79,90</span>
+            </footer>
+          </Product>
+        ))}
+      </HomeContainer>
     </>
   )
 }
