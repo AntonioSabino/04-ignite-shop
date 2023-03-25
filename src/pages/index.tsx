b73b4ac6bd6906ee8c31e22f841c3fb4d4eced7e
Antonio Sabino <aslimajr@outlook.com>
2023-03-25 13:25:25 -0300

    feat: criando o carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,27 @@
-import { styled } from '@/styles'
 import { HomeContainer, Product } from '@/styles/pages/home'
 import Image from 'next/image'
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 import camiseta1 from '../assets/camisetas/1.png'
 import camiseta2 from '../assets/camisetas/2.png'
 import camiseta3 from '../assets/camisetas/3.png'
-
-const camisetas = [camiseta1, camiseta2, camiseta3]
+import camiseta4 from '../assets/camisetas/3.png'
+const camisetas = [camiseta1, camiseta2, camiseta3, camiseta4]
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
     <>
-      <HomeContainer>
+      <HomeContainer ref={sliderRef} className="keen-slider">
         {camisetas.map((camiseta, index) => (
-          <Product key={index}>
+          <Product key={index} className="keen-slider__slide">
             <Image src={camiseta} width={520} height={480} alt="" />
             <footer>
               <strong>Camiseta X</strong>
